6b97ffb51980d8e44c37a2293a0ae7dc64f99d63
John Petersson <johnpetersson@gmail.com>
2017-06-24 15:43:45 +0200

    Simple description displayed as h1 when lacking readme.

--- client/app.js
+++ client/app.js
@@ -2,8 +2,8 @@ let p5ProjectsPath = './projects/p5/';
 let sketch1Folder = 'blobs/';
 let sketch2Folder = 'perlin/';
 let sketch3Folder = 'code-rain/';
-let sketchName = 'index.html';
-let readmeName = "README.md";
+let sketchNameStandard = 'index.html';
+let readmeNameStandard = "README.md";
 
 let currentSketchIndex = 1;
 let sketchContainerName = 'sketch-container';
@@ -12,30 +12,27 @@ let sketches = [
 {
 	name: 'blobs',
 	folderPath: p5ProjectsPath + sketch1Folder,
-	entryFileName: sketchName,
-	readmeFileName: readmeName,
-	description: '# Simple blob-eat-blob game'
+	entryFileName: sketchNameStandard,
+	readmeFileName: readmeNameStandard,
+	description: 'Simple blob-eat-blob game'
 }, {
 	name: 'perlin',
 	folderPath: p5ProjectsPath + sketch2Folder,
-	entryFileName: sketchName,
-	readmeFileName: readmeName,
-	description: '# Perlin noise pattern generating experiment'
+	entryFileName: sketchNameStandard,
+	readmeFileName: readmeNameStandard,
+	description: 'Perlin noise pattern generating experiment'
 },
 {
 	name: 'code-rain',
 	folderPath: p5ProjectsPath + sketch3Folder,
-	entryFileName: sketchName,
-	readmeFileName: readmeName,
-	description: '# Matrix style code rain'
+	entryFileName: sketchNameStandard,
+	readmeFileName: readmeNameStandard,
+	description: 'Matrix style code rain'
 }
 ];
 
+// Display a sketch
 displayOnly(sketches[1]);
-
-// testGithubAPI();
-
-// sketches.forEach (appendToDOM);
 
 function appendToDOM(sketch) {
 	let iframe = document.createElement('iframe');
@@ -50,8 +47,10 @@ function appendToDOM(sketch) {
 	getRawTextContentsOfFile(readmePath, function() {
 		let rawText = this.responseText;
 		if (this.status === 404 ) {
-			// No readme file, use description in sketches array
-			rawText = sketch.description;
+			// No readme file, use description in sketches array, with html formatting
+			let descriptionHTML = getSimpleHTMLDescription(sketch);
+			overlayWithInfoPanel(sketchContainerNode, descriptionHTML);
+			return;
 		}
 
 		// Convert sketch info markdown to HTML and add it as an overlay
@@ -59,8 +58,7 @@ function appendToDOM(sketch) {
 			let descriptionHTML = this.responseText;
 			if (descriptionHTML.indexOf('API rate limit exceeded') !== -1) {
 				// Github requests for markdown conversion has reached it's limit
-				// descriptionHTML = sketch.description;
-				descriptionHTML = '';
+				descriptionHTML = getSimpleHTMLDescription(sketch);
 			}
 			// 'this' refers to the http response  
 			overlayWithInfoPanel(sketchContainerNode, descriptionHTML);
@@ -126,6 +124,8 @@ function testGithubAPI() {
 }
 
 function getHTMLFromMarkdown(markupText, callback) {
+	// Use the github api to convert markdown to html
+
 	let http = new XMLHttpRequest();
 	let url = 'https://api.github.com' + '/markdown';
 	let params = {"text": markupText};
@@ -134,24 +134,9 @@ function getHTMLFromMarkdown(markupText, callback) {
 
 	// Send proper header with request
 	http.setRequestHeader("Content-type", "application/json");
-
 	http.onload = callback;
-	// http.onreadystatechange = function() {
-	// 	if (this.readyState == 4 && this.status == 200) {
-	// 		alert(http.responseText);
-	// 	}
-	// };
 	http.send(JSON.stringify(params));
 }
-
-// function handleResponse() {
-// 	// let responseObj = JSON.parse(this.responseText);
-// 	// console.log(responseObj);
-// 	console.log(this.responseText);
-// 	// if (this.readyState == 4 && this.status == 200) {
-// 	// 		alert(http.responseText);
-// 	// 	}
-// }
 
 function getRawTextContentsOfFile(path, callback) {
 	let httpRequest = new XMLHttpRequest();
@@ -161,6 +146,10 @@ function getRawTextContentsOfFile(path, callback) {
 	httpRequest.open('GET', path);
 	httpRequest.onload = callback;
 	httpRequest.send();
+}
+
+function getSimpleHTMLDescription(sketch) {
+	return '<h1>' + sketch.description + '</h1>';
 }
 
 let prevButton = document.getElementById("previous");
