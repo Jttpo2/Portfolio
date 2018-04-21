f8d1e22667fd0de677b47e81ff1273dfef55cc48
John Petersson <johnpetersson@gmail.com>
2018-04-22 00:37:41 +0200

    Gulpfile updated.

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ gulp.task('default', ['watch']);
 gulp.task('watch', function() {
 	livereload.listen();
 	gulp.watch('**/*.scss', ['sass', 'reload']);
-	gulp.watch('public/**/*.js', ['reload']);
-	gulp.watch('public/**/*.html', ['reload']);
+	gulp.watch('dist/**/*.js', ['reload']);
+	gulp.watch('dist/**/*.html', ['reload']);
 });
 
 gulp.task('reload', function() {
@@ -18,7 +18,7 @@ gulp.task('reload', function() {
 gulp.task('sass', function () {
 	gulp.src('./sass/**/*.scss')
 	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./public'));
+	.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build', ['sass']);
