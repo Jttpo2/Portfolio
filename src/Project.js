955d8adcfc9837c730f548ce43931fb4c7d333ca
John Petersson <johnpetersson@gmail.com>
2018-06-05 00:54:44 +0200

    Trying to focus on iframe on load

--- src/Project.js
+++ src/Project.js
@@ -6,16 +6,24 @@ const defaultWidth = 500;
 const defaultHeight = 300;
 
 export default class Project extends React.Component {
+  componentDidMount() {
+    this.frame.onload = () => {
+      console.log('Frame loaded');
+      this.frame.contentWindow.focus();
+    };
+  }
+
   render() {
     return (
       <BorderlessIframe
         src={this.props.project.url}
         width={this.props.fullScreen ? window.innerWidth : defaultWidth}
-        height={this.props.fullScreen ? window.innerHeight : defaultHeight}/>
-    );
+        height={this.props.fullScreen ? window.innerHeight : defaultHeight}
+        ref={(f) => this.frame = f}/>
+      );
+    }
   }
-}
 
-const BorderlessIframe = styled.iframe`
+  const BorderlessIframe = styled.iframe`
   border: none;
-`;
+  `;
