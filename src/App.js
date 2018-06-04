b7568784b3717e2ac3c1891a20befcd060d3ff23
John Petersson <johnpetersson@gmail.com>
2018-06-04 22:38:43 +0200

    Submodule mess

--- src/App.js
+++ src/App.js
@@ -3,16 +3,67 @@ import styled from 'styled-components';
 
 import ProjectsSlideShow from './ProjectsSlideshow.js';
 
-const projects = [
-  'http://singit.netlify.com',
-  'http://dadgad-tuner.netlify.com'
-]
+// const projectsPath = './projects/';
+// const p5Path = 'p5/';
+// const unityPath = 'unity/';
+// const sketchNameStandard = 'index.html';
+// const readmeNameStandard = "README.md";
+// const sketches = [
+// 	{
+// 		name: 'perlin',
+// 		folderPath: projectsPath + p5Path + 'perlin/',
+// 		entryFileName: sketchNameStandard,
+// 		readmeFileName: readmeNameStandard,
+// 		description: 'Perlin noise pattern generating experiment'
+// 	},
+// 	{
+// 		name: 'flowers',
+// 		folderPath: projectsPath + p5Path + 'flowers/',
+// 		entryFileName: sketchNameStandard,
+// 		readmeFileName: readmeNameStandard,
+// 		description: 'Simulation of tulips in the wind'
+// 	},
+// 	{
+// 		name: 'flockheart',
+// 		folderPath: projectsPath + unityPath + 'flockheart/',
+// 		entryFileName: sketchNameStandard,
+// 		readmeFileName: readmeNameStandard,
+// 		description: 'Flockheart'
+// 	},
+// 	{
+// 		name: 'blobs',
+// 		folderPath: projectsPath + p5Path + 'blobs/',
+// 		entryFileName: sketchNameStandard,
+// 		readmeFileName: readmeNameStandard,
+// 		description: 'Simple blob-eat-blob game'
+// 	},
+// 	{
+// 		name: 'code-rain',
+// 		folderPath: projectsPath + p5Path + 'code-rain/',
+// 		entryFileName: sketchNameStandard,
+// 		readmeFileName: readmeNameStandard,
+// 		description: 'Matrix style code rain'
+// 	}
+// ];
+
+// const testSketchPath = ;
+// console.log(testSketchPath);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.projects = [
+      './projects/p5/perlin/index.html',
+      'http://singit.netlify.com',
+      'http://dadgad-tuner.netlify.com'
+    ];
+  }
+
   render() {
     return (
       <Container className="App">
-        <ProjectsSlideShow projects={projects}/>
+        <ProjectsSlideShow projects={this.projects}/>
       </Container>
     );
   }
