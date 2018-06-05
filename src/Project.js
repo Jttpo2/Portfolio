bc1204447b681251f1941b33d4c6bb0bcc20bbe3
John Petersson <johnpetersson@gmail.com>
2018-06-05 19:35:17 +0200

    Reload iframe on resize

--- src/Project.js
+++ src/Project.js
@@ -6,11 +6,27 @@ const defaultWidth = 500;
 const defaultHeight = 300;
 
 export default class Project extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
   componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+
     this.frame.onload = () => {
       console.log('Frame loaded');
       this.frame.contentWindow.focus();
     };
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions)
+  }
+
+  updateDimensions() {
+    this.forceUpdate();
   }
 
   render() {
